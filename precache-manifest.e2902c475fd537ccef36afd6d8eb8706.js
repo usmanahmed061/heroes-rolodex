e5337a52e4718d76a7b1143103e06c5d2e89fdc9
Usman Ahmed <usmanahmed061@outlook.com>
2020-07-06 14:03:27 +0500

    Updates

---
+++ precache-manifest.e2902c475fd537ccef36afd6d8eb8706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16b501d83629a22ea36090d9bc1f19f",
+    "url": "/heroes-rolodex/index.html"
+  },
+  {
+    "revision": "cf32ec2f4e2463d016e7",
+    "url": "/heroes-rolodex/static/css/main.a17d1da2.chunk.css"
+  },
+  {
+    "revision": "171817baa2912415cb01",
+    "url": "/heroes-rolodex/static/js/2.c6d1bc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heroes-rolodex/static/js/2.c6d1bc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf32ec2f4e2463d016e7",
+    "url": "/heroes-rolodex/static/js/main.2213f094.chunk.js"
+  },
+  {
+    "revision": "3eff67f4bb97657b003b",
+    "url": "/heroes-rolodex/static/js/runtime-main.a810c5a8.js"
+  }
+]);
